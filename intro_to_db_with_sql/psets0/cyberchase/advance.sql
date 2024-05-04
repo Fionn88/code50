91edd8a31a4c57d08e4631b14a72ff580c2a9efc
Yuan-Heng,Kuo <coco8802231225@gmail.com>
2024-05-05 01:46:17 +0800

    feat: keep solving the views problem

--- intro_to_db_with_sql/psets0/cyberchase/advance.sql
+++ intro_to_db_with_sql/psets0/cyberchase/advance.sql
@@ -4,7 +4,15 @@ Write a SQL query to find the titles of episodes that have aired during the holi
     Try to find a better solution than LIKE if you can!
 ```
 
+SELECT "title" FROM "episodes" WHERE air_date LIKE "_____12___";
+
+SELECT "title"
+FROM "episodes"
+WHERE instr(air_date, 12) = 6;
+
 ```
 Write a SQL query to find, for each year, the first day of the year that PBS released a Cyberchase episode.
     Your query should output a table with two columns, one for the year and one for the earliest month and day an episode was released that year.
 ```
+
+SELECT "title",strftime('%Y', air_date) as year, "air_date" FROM "episodes" GROUP BY year; 
